2fbbea8ae351fc83e290e228b30b3fb372791b06
fatoly <fatoly@gmail.com>
2017-06-15 00:03:03 +0700

    3

--- Node v1.1/NodeKernel.h
+++ Node v1.1/NodeKernel.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Structures.h"
-
 #include <algorithm>
 
 class NodeKernel
@@ -8,30 +7,31 @@ class NodeKernel
 	private:
 		GlobalSystem Global;													//Глобальная структура для главного потока
 		ThreadSystem Threads;													//Структура для потоков
-		USUSystem Usu;															//Структура для подсистемы УСУ
+		UsuSystem Usu;															//Структура для подсистемы УСУ
 		/*Приватные функции в этом классе*/
-		void Calc(int startNode, int endNode);									//Рекурсия для просчета маршрутов
-		void MapGlobalInit(char* fileName);										//Инициализация глобальной карты
-		void MapLocalInit(char* fileName);										//Инициализация локальной карты
-		void CreatePacGlobal(std::vector<int> PacGlobal);						//Формирует пакет на отправку в сеть узлов и шлет на нужную шину
-		void CreatePacLocal(std::vector<int> PacLocal);							//Формирует пакет на отправку в УСУ и шлет на нужную шину
-		int HowManySheets();													//Возвращает кол-во шин на эмулируемом узле (сколько надо создать потоков итд)
-		int ShowSheet(int from, int where);										//Возвращает на какую шину данных сейчас надо отправить пакет
-		void Events(std::vector<int> OnePac);									//обрабатывает пакет в соответствии с событиями
-		void CreateNewEvent(std::string NewEvent);								//Создать новое событие
-		bool CompareEvents(int key);											//Сравнить два события
-		void ExecuteEvent(int key);												//Выполнить событие
-		void GlobalEvent(std::vector<int> Package);								//Обработчик пакетов на отправку в сеть
-		void LocalEvent(std::vector<int> Package);								//Обработчик пакетов пришедших из подсистемы
-		int ShowWays(int from, int where);										//Возвращает кол-во путей от одного узла до другого, хз зачем это надо но пусть будет.
-		int CreateMeshThread();													//Возвращает по какому i найти HANDLE потока в массиве хэндлов Threads	
+		void	Calc(int startNode, int endNode);								//Рекурсия для просчета маршрутов
+		void	MapGlobalInit(char* fileName);									//Инициализация глобальной карты
+		void	MapLocalInit(char* fileName);									//Инициализация локальной карты
+		void	CreatePacGlobal(std::vector<int> PacGlobal);					//Формирует пакет на отправку в сеть узлов и шлет на нужную шину
+		void	CreatePacLocal(std::vector<int> PacLocal);						//Формирует пакет на отправку в УСУ и шлет на нужную шину
+		int		HowManySheets();												//Возвращает кол-во шин на эмулируемом узле (сколько надо создать потоков итд)
+		int		ShowSheet(int from, int where);									//Возвращает на какую шину данных сейчас надо отправить пакет
+		void	Events(std::vector<int> OnePac);								//обрабатывает пакет в соответствии с событиями
+		void	CreateNewEvent(std::string NewEvent);							//Создать новое событие
+		bool	CompareEvents(int key);											//Сравнить два события
+		void	ExecuteEvent(int key);											//Выполнить событие
+		void	GlobalEvent(std::vector<int> Package);							//Обработчик пакетов на отправку в сеть
+		void	LocalEvent(std::vector<int> Package);							//Обработчик пакетов пришедших из подсистемы
+		int		ShowWays(int from, int where);									//Возвращает кол-во путей от одного узла до другого, хз зачем это надо но пусть будет.
+		int		CreateMeshThread();												//Возвращает по какому i найти HANDLE потока в массиве хэндлов Threads	
 	public:
 		/*Публичные функции этого класса*/
-		void PushPacMesh(char* filename);										//Запушить из текстового файла в структуры для шин (для эмуляции)
-		void PushPacUSU(char* filename);										//Запушить из текстового файла в структуры для шины УСУ (значения датчиков итд) (для эмуляции)
-		bool PauseThreads(int mode);											//1 - все, 2 - только шины. Возвращает 1 если все ровно, 0 если уже на паузе
-		bool ContinueThreads(int mode);											//1 - все, 2 - только шины. Возвращает 1 если все ровно, 0 если уже работают
-		int SystemState(int Parametr);											//Возвращает состояние системы (сколько потоков открыто, сколько пакетов в буферах итд)
+		void	PushPacMesh(char* filename);									//Запушить из текстового файла в структуры для шин (для эмуляции)
+		void	PushPacUSU(char* filename);										//Запушить из текстового файла в структуры для шины УСУ (значения датчиков итд) (для эмуляции)
+		bool	PauseThreads(int mode);											//1 - все, 2 - только шины. Возвращает 1 если все ровно, 0 если уже на паузе
+		bool	ContinueThreads(int mode);										//1 - все, 2 - только шины. Возвращает 1 если все ровно, 0 если уже работают
+		int		SystemState(int Parametr);										//Возвращает состояние системы (сколько потоков открыто, сколько пакетов в буферах итд)
+		
 		NodeKernel(char* Local, char* Global, int dream, int now);				//Конструктор класса. На вход имена двух текстовых файлов, интервал сна, номер узла
 		~NodeKernel();
 		//Потоки
--- Node v1.1/Structures.h
+++ Node v1.1/Structures.h
@@ -7,7 +7,6 @@
 struct Rules
 {
 	std::vector<int[2]> Address;											//Адреса, с которых требуются данные для исполнения сценария (Узел и УСУ)		
-																			//vector<> - массив, где каждый элемент соответствует адресу с которого ожидаются данные для исполнения сценария
 	std::vector<std::multimap<std::string, std::multimap <bool, double[2]>>> Values;//Структура для объявления значений для самого сценария. Индексы соответствуют с Adress
 };
 /*Событие*/
@@ -21,6 +20,9 @@ struct EventSystem
 /*Самая главная структура для узла*/
 struct GlobalSystem
 {
+	int dream;																//Интервал сна
+	int now;																//эмулируемый узел (порядковый номер)
+	std::string name;														//Имя узла
 	std::vector<std::vector<int>> mapResult;								//Структура для маршрутов
 	std::vector<int> visitedNodes;											//Структура для функции Calc
 	std::map<int, std::map<int, int>> MapLocal;								//Структура для мапы БСУ
@@ -29,8 +31,6 @@ struct GlobalSystem
 	HANDLE hProcess;														//Мютекс для главной структуры GenHandler
 	std::vector<std::vector<int>> Pass;										//Структура Pass для ослеживания пропущенных пакетов
 	HANDLE hPass;															//Мьютекс для структуры Pass
-	int dream;																//Интервал сна
-	int now;																//эмулируемый узел
 	std::vector<EventSystem> Events;										//Массив структур Event
 	std::vector<int> PacEvent;												//Буфер для функции Event
 };
@@ -44,11 +44,11 @@ struct ThreadSystem
 	std::vector<HANDLE> hMeshIn;											//Массив мьютексов для доступа в структуру MeshIn
 };
 /*Это все что связанно с сетью УСУ данного узла*/
-struct USUSystem
+struct UsuSystem
 {
-	std::vector<std::vector<int>> USUOut;									//Структура для отправки пакетов в сеть УСУ
-	HANDLE hUSUOut;															//Мьютекс для этой структуры
-	std::vector<std::vector<int>> USUIn;									//Структура входных данных с сети УСУ
-	HANDLE hUSUIn;															//Мьютекс для этой структуры
-	HANDLE hUSUThread;														//Хэндл потока работающего с сетью УСУ
+	std::vector<std::vector<int>> UsuOut;									//Структура для отправки пакетов в сеть УСУ
+	HANDLE hUsuOut;															//Мьютекс для этой структуры
+	std::vector<std::vector<int>> UsuIn;									//Структура входных данных с сети УСУ
+	HANDLE hUsuIn;															//Мьютекс для этой структуры
+	HANDLE hUsuThread;														//Хэндл потока работающего с сетью УСУ
 };
